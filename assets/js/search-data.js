cdffcbb12c871eabcff6da6ccd1ec19c3b6fc600
NimaEsr <95144311+NimaEsr@users.noreply.github.com>
2026-08-21 00:24:21 +0000

    Deploying to gh-pages from @ ai-workshop-vg/highstakes@897f6d15b49887e468cdd184d27a74ade674346b 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,55 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/highstakes/";
+    },
+  },{id: "nav-schedule",
+          title: "Schedule",
+          description: "Full-day workshop schedule · NeurIPS 2026, Atlanta · December 2026",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/highstakes/schedule/";
+          },
+        },{id: "nav-call-for-papers",
+          title: "Call for Papers",
+          description: "Submission guidelines, topics of interest, and review process",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/highstakes/cfp/";
+          },
+        },{id: "news-workshop-proposal-submitted-to-neurips-2026-atlanta-satellite-call-for-papers-coming-july-2026-stay-tuned",
+          title: 'Workshop proposal submitted to NeurIPS 2026 (Atlanta satellite). Call for papers coming July...',
+          description: "",
+          section: "News",},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
